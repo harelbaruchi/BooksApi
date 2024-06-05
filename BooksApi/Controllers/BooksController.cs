e8a0a0ae0ebd1320dcfdda338181136e1782ceb6
harelbaruchi <harelbaruchi@gmail.com>
2024-06-05 09:21:40 +0300

    Add project files.

---
+++ BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Xml;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BooksApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+
+namespace BooksApi.Controllers
+{
+
+[Route("api/[controller]")]
+[ApiController]
+public class BooksController : ControllerBase
+    {
+       
+      private readonly string _filePath = "";
+      
+        public BooksController(IWebHostEnvironment env)
+        {
+            _filePath = Path.Combine(env.ContentRootPath, "Data","Books.xml");
+        }
+
+        // GET: api/Books
+        [HttpGet]
+    public IEnumerable<Book> Get()
+    {
+        return ParseBooksFromFile();
+    }
+
+        // GET: api/Books/{isbn}
+        [HttpGet("{isbn}")]
+        public Book Get(string isbn)
+    {
+        var books = ParseBooksFromFile();
+        return books.FirstOrDefault(b => b.Isbn == isbn);
+    }
+
+        // POST: api/Books
+        [HttpPost]
+        public void Post(Book book)
+    {
+        var books = ParseBooksFromFile();
+        books.Add(book);
+        SaveBooksToFile(books);
+    }
+
+        // PUT: api/Books/{isbn}
+        [HttpPut("{isbn}")]
+        public void Put(string isbn, Book book)
+    {
+        var books = ParseBooksFromFile();
+        var index = books.FindIndex(b => b.Isbn == isbn);
+        if (index > -1)
+        {
+            books[index] = book;
+            SaveBooksToFile(books);
+        }
+    }
+
+        // DELETE: api/Books/{isbn}
+        [HttpDelete("{isbn}")]
+        public void Delete(string isbn)
+    {
+        var books = ParseBooksFromFile();
+        books.RemoveAll(b => b.Isbn == isbn);
+        SaveBooksToFile(books);
+    }
+
+        private List<Book> ParseBooksFromFile()
+        {
+            var books = new List<Book>();
+            if (System.IO.File.Exists(_filePath))
+            {
+                var xmlDoc = new XmlDocument();
+                xmlDoc.Load(_filePath);
+
+                var bookstoreNode = xmlDoc.DocumentElement; // Assuming root element is "bookstore"
+
+                foreach (XmlNode bookNode in bookstoreNode.ChildNodes)
+                {
+                    if (bookNode.Name == "book")
+                    {
+                        var book = new Book
+                        {
+                            Isbn = bookNode["isbn"]?.InnerText ??"",
+                            Title = bookNode["title"]?.InnerText??"",
+                            Language = bookNode["title"]?.Attributes["lang"]?.Value ?? "", // Check for language attribute
+                            Authors = new List<string>(),
+                            Category = bookNode.Attributes["category"]?.Value ?? "", // Check for category attribute
+                            Cover = bookNode.Attributes["cover"]?.Value ?? "" // Check for cover attribute
+                        };
+
+                        foreach (XmlNode authorNode in bookNode.SelectNodes("author"))
+                        {
+                            book.Authors.Add(authorNode.InnerText);
+                        }
+
+                        var yearNode = bookNode.SelectSingleNode("year");
+                        book.Year = int.Parse(yearNode.InnerText);
+
+                        var priceNode = bookNode.SelectSingleNode("price");
+                        book.Price = decimal.Parse(priceNode.InnerText);
+
+                        books.Add(book);
+                    }
+                }
+            }
+            return books;
+        }
+
+        private void SaveBooksToFile(List<Book> books)
+    {
+        var xmlDoc = new XmlDocument();
+        var rootNode = xmlDoc.CreateElement("bookstore");
+        xmlDoc.AppendChild(rootNode);
+
+        foreach (var book in books)
+        {
+            var bookNode = xmlDoc.CreateElement("book");
+            rootNode.AppendChild(bookNode);
+
+
+            var isbnNode = xmlDoc.CreateElement("isbn");
+            isbnNode.InnerText = book.Isbn;
+            bookNode.AppendChild(isbnNode);
+
+            var titleNode = xmlDoc.CreateElement("title");
+            titleNode.InnerText = book.Title;
+            titleNode.SetAttribute("lang", book.Language);
+            bookNode.AppendChild(titleNode);
+
+            foreach (var author in book.Authors)
+            {
+                var authorNode = xmlDoc.CreateElement("author");
+                authorNode.InnerText = author;
+                bookNode.AppendChild(authorNode);
+            }
+
+            var yearNode = xmlDoc.CreateElement("year");
+            yearNode.InnerText = book.Year.ToString();
+            bookNode.AppendChild(yearNode);
+
+            var priceNode = xmlDoc.CreateElement("price");
+            priceNode.InnerText = book.Price.ToString();
+            bookNode.AppendChild(priceNode);
+        }
+
+        xmlDoc.Save(_filePath);
+    }
+}
+}
+
+
+
+
---
+++ BooksApi/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BooksApi.Models
+{
+    public class Book
+    {
+        public string Isbn { get; set; } = "";
+        [Column(TypeName = "nvarchar(100)")]
+        public string Title { get; set; } = "";
+        [Column(TypeName = "nvarchar(100)")]
+        public string Language { get; set; } = ""; // Extracted from title attribute
+        [Column(TypeName = "nvarchar(16)")]
+        public List<string> Authors { get; set; } = [];
+        public int Year { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public decimal Price { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Category { get; set; } = "";
+        [Column(TypeName = "nvarchar(100)")]
+        public string Cover { get; set; } = "";
+        
+    }
+}
